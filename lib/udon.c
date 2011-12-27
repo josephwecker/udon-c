ca7cfeae56ed4e7db38b33362c1430bd345f3c3d
Joseph Wecker <joseph.wecker@gmail.com>
2011-12-26 18:19:38 -0800

    Basic node structs re-added and parser being built based on state machine as a by-hand experiment. Seems to compile and more or less work at the high level.

--- lib/udon.c
+++ lib/udon.c
@@ -3,8 +3,11 @@
 
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <setjmp.h>
 #include "udon.h"
 
+
+char tmp_test[] = "some string or other...";
 char udon_global_error_msg[128];
 UdonError udon_global_error = UDON_NO_ERROR;
 
@@ -47,22 +50,151 @@ UdonParser *udon_new_parser_from_file(char *filename) {
     return p;
 }
 
-int udon_parse(UdonParser *p) {
-}
-
 int udon_reset_parser(UdonParser *p) {
-    p->curr = p->buffer;
-    p->end = &(p->buffer[p->size - 1]);
+    p->curr          = p->buffer;
+    p->end           = &(p->buffer[p->size - 1]);
 
-    p->qcurr = (uint64_t *) p->buffer;
-    p->qsize = p->size >> 3; // size in 64b chunks
-    p->qend = &(p->qcurr[p->qsize - 1]);
+    p->qcurr         = (uint64_t *) p->buffer;
+    p->qsize         = p->size >> 3; // size in 64b chunks
+    p->qend          = &(p->qcurr[p->qsize - 1]);
 
     p->curr[p->size] = 0; // last chance null terminator, just in case.
+
+    p->column        = 1;
+    p->line          = 1;
+    p->error         = UDON_NO_ERROR;
+    p->state         = UDON_NOT_STARTED;
 }
 
 int udon_free_parser(UdonParser *p) {
 
+}
+
+ /* warnings       ... accumulate in parser
+ *  grammar errors ... specialized error message, propagate finish.
+ *  system errors  ... shortcircuit and propagate
+ *  eof            ... explicitly handled
+ *  unexpected eof ... grammar error
+ */
+static jmp_buf setjmp_buf;
+
+int udon_parse(UdonParser *p) {
+    int retval = setjmp(setjmp_buf);
+    if(retval) return retval;
+
+    p->result = gm__root(p);
+
+    return 0;
+}
+
+static void *gm__root(UdonParser *p) {
+    void *s = NULL;
+    s = gm__node(p, 1);
+    return s;
+}
+
+static inline void *gm__node(UdonParser *p, int skip_to_child_shortcut) {
+    void *s = NULL;
+    if(skip_to_child_shortcut) goto s_child_shortcut;
+    int _local_inline = 0;
+    uint64_t _local_ibase  = p->column;
+    uint64_t _local_ipar   = p->column - 1;
+
+s_initialize:
+    // TODO: eof check
+    switch(*(p->curr)) {
+        case ' ':   // value
+        case '\t':  // value
+            ADVANCE_COLUMN();
+            s = udon_new_value_node(p);
+            goto s_value;
+        case '\n':  // value
+            ADVANCE_LINE();
+            s = udon_new_value_node(p);
+            goto s_value;
+        case ':':   // attr
+            ADVANCE_COLUMN();
+            s = udon_new_full_node(p);
+            goto s_attribute;
+        case '[':
+            s = udon_new_full_node(p);
+            goto s_identity__id;
+        case '.':   // class
+            s = udon_new_full_node(p);
+            goto s_identity__class;
+        case '(':   // protected
+            s = udon_new_full_node(p);
+            ((UdonFullNode *)s)->name = (char *)gm__protected(p);
+            goto s_identity;
+        case '|':   // child
+            s = udon_new_full_node(p);
+            goto s_child__node;
+        case '{':   // embed
+            //udon_error_p(p, UDON_SYSTEM_ERROR, __FILE__, __LINE__, "Embedded nodes not yet supported."); longjmp(setjmp_buf, (int)UDON_SYSTEM_ERROR);
+            UDON_ERR("Embedded nodes not yet supported.");
+        default:
+            s = udon_new_full_node(p);
+            ((UdonFullNode *)s)->name = (char *)gm__label(p);
+            goto s_identity;
+    }
+
+s_identity:
+    switch(*(p->curr)) {
+        case ' ':   // leading
+        case '\t':  // leading
+            ADVANCE_COLUMN();
+            goto s_identity;
+        case '[':   // id
+s_identity__id:
+            ((UdonFullNode *)s)->id = (char *)gm__protected(p);
+            goto s_identity;
+        case '.':   // class
+s_identity__class:
+            ADVANCE_COLUMN();
+            // TODO: fix so that it allocates an
+            //       UdonList instead of string.
+            ((UdonFullNode *)s)->classes = udon_do_list_append( (UdonList *)(((UdonFullNode *)s)->classes), (UdonList *)gm__label(p));
+            goto s_identity;
+        default:   // child
+            goto s_child;
+    }
+
+s_child_shortcut:
+    s = udon_new_full_node(p);
+    goto s_child;
+
+s_attribute:
+s_value:
+s_child:
+s_child__node:
+    return s;
+}
+
+static inline void *udon_new_value_node(UdonParser *p) {
+    void *res;
+    fprintf(stderr, "udon_new_value_node()");
+    if( (res = (UdonFullNode *)udon_malloc(sizeof(UdonFullNode))) == NULL)
+        UDON_SYSERR("Couldn't allocate memory for a value Udon node.");
+    return res;
+}
+
+static inline void *udon_new_full_node(UdonParser *p) {
+    void *res;
+    fprintf(stderr, "udon_new_full_node()");
+    UDON_SYSERR("Sorry, don't like you.");
+    if( (res = (UdonFullNode *)udon_malloc(sizeof(UdonFullNode))) == NULL)
+        UDON_SYSERR("Couldn't allocate memory for a full Udon node.");
+    return res;
+}
+
+static inline void *gm__protected(UdonParser *p) {
+    fprintf(stderr, "protected()");
+    return &tmp_test;
+}
+
+static inline void *gm__label(UdonParser *p) {
+    fprintf(stderr, "label()");
+    return &tmp_test;
 }
 
 
--- lib/udon.h
+++ lib/udon.h
@@ -21,16 +21,84 @@ extern "C" {
         UDON_NO_ERROR            = 0,
         UDON_OUT_OF_MEMORY_ERROR = EX_OSERR,
         UDON_BAD_FILE_ERROR      = EX_NOINPUT,
-        UDON_READ_FILE_ERROR     = EX_DATAERR
+        UDON_READ_FILE_ERROR     = EX_DATAERR,
+        UDON_SYSTEM_ERROR,
+        UDON_GRAMMAR_ERROR
     };
     typedef enum UdonError UdonError;
 
+    // Global error state - declared here but defined in udon.c
     extern UdonError udon_global_error;
     extern char udon_global_error_msg[128];
+
+    static inline void udon_error(UdonError type, char *msg, ...) {
+        va_list argp;
+        va_start(argp, msg);
+        vsnprintf(udon_global_error_msg, 128, msg, argp);
+        udon_global_error = type;
+    }
+
+
+    void udon_emit_error(FILE *outf) {
+        fprintf(outf, "%s\n", udon_global_error_msg, &udon_global_error_msg);
+    }
+
+    int udon_error_value() {
+        return (int)udon_global_error;
+    }
+
+
+
+// --- Structures for holding results etc. ---
+
+    struct UdonList {
+        void             *v;
+        struct UdonList  *next;
+    }; typedef struct UdonList UdonList;
+
+    enum UdonType {
+        NODE,
+        TEXT,
+        ATTR_VALUE,
+        CLASS,
+        PARENT,
+        COMMENT,
+        FULL
+    };
+
+    struct UdonNode {
+        UdonList         _base;         // Acts like a linked list
+        enum UdonType    node_type;     // Node type, from enum above.
+        uint64_t         source_line;   // Line where the parser encountered it.
+        uint64_t         source_col;    // Column on the line where the parser encountered it.
+    }; typedef struct UdonNode UdonNode;
+
+    /*struct UdonComment {
+        UdonNode         _base;         // Extends simple node
+        UdonNode         *children;     // Linked list of children
+    }; typedef struct UdonComment UdonComment;*/
+
+    typedef struct _UdonAttributes {
+        UdonList         *keys;         // Linked list of keys
+        struct hsearch_data *hash_table;   // Actual hash table
+        uint64_t         size;          // How many attributes defined so far
+        uint64_t         num_allocated; // If size surpasses this, a new hash table must be created.
+    } UdonAttributes;
+
+    struct UdonFullNode {
+        struct UdonNode  _base;         // Extend basic node
+        UdonNode         *children;     // (same spot as UdonComment) - children linked-list
+        char             *name;         // This is the "tag"
+        char             *id;           // If specified, the ID string
+        UdonList         *classes;      // Linked list of classes
+        UdonAttributes   attributes;    // See structure above
+    };
+    typedef struct UdonFullNode UdonFullNode;
 
 
 // --- Overall state of the parser ---
     enum UdonParseState {
+        UDON_NOT_STARTED,
         UDON_FINISHED_OK,
         UDON_FINISHED_WARNINGS,
         UDON_ABORTED_ERROR
@@ -44,19 +112,55 @@ extern "C" {
         ssize_t          size;
         char             *filename; // optional, of course
 
-        // --- Current State ---
+        // --- Result State ---
         void             *result;
+        UdonError        error;
+        char             error_message[256];
+        UdonList         warnings;
+
+        // --- Current State ---
         UdonParseState   state;
+        uint64_t         line;
+        uint64_t         column;
 
         // --- Generally Opaque ---
+        //void             *local_result;
         char             *curr;
         uint64_t         *qcurr;
         char             *end;
         uint64_t         *qend;
-        ssize_t          qsize; // Automatically calculated, for quickscans.
+        ssize_t          qsize;  // Automatically calculated, for quickscans.
+        char             *alpha; // Used for accumulating
     };
     typedef struct UdonParser UdonParser;
 
+
+    // --- USED INTERNALLY ---
+#define ADVANCE_COLUMN() p->column ++; p->curr ++;
+#define ADVANCE_LINE()   p->column = 1; p->line ++; p->curr ++;
+
+    static inline void udon_error_p(UdonParser *p, UdonError type, char *filename, int lineno, char *msg, ...) {
+        va_list argp;
+        va_start(argp, msg);
+        // TODO: Get source line / filename for system errors, and those plus
+        // udon-source line / filename for grammar errors.
+        vsnprintf(udon_global_error_msg, 128, msg, argp);
+        vsnprintf(p->error_message, 256, msg, argp);
+        udon_global_error = type;
+        p->error = type;
+        p->state = UDON_ABORTED_ERROR;
+    }
+
+#define UDON_SYSERR(msg, ...) { udon_error_p(p, UDON_SYSTEM_ERROR, __FILE__, __LINE__, msg, ##__VA_ARGS__); longjmp(setjmp_buf, (int)UDON_SYSTEM_ERROR); }
+
+#define UDON_ERR(msg, ...) udon_error_p(p, UDON_GRAMMAR_ERROR, __FILE__, __LINE__, msg, ##__VA_ARGS__); longjmp(setjmp_buf, (int)UDON_GRAMMAR_ERROR);
+
+    static inline UdonList *udon_do_list_append(UdonList *list, UdonList *new_value) {
+        if(list) list->next = new_value;
+        return new_value;
+    }
+
+    // --- MAIN API INTERFACE ---
     UdonParser *udon_new_parser_from_buffer(char *buffer, ssize_t length);
     UdonParser *udon_new_parser_from_file(char *filename);
     int udon_parse(UdonParser *parser);
@@ -79,21 +183,15 @@ extern "C" {
 #define udon_free(ptr,size)     free(ptr)
 #endif
 
-    // --- Error / Warning handling ---
-    static inline void udon_error(UdonError type, char *msg, ...) {
-        va_list argp;
-        va_start(argp, msg);
-        vsnprintf(udon_global_error_msg, 128, msg, argp);
-        udon_global_error = type;
-    }
 
-    static inline void udon_emit_error(FILE *outf) {
-        fprintf(outf, "%s\n", udon_global_error_msg, &udon_global_error_msg);
-    }
+    // --- Opaque ---
+    static        void *gm__root     (UdonParser *p);
+    static inline void *gm__node     (UdonParser *p, int skip_to_child_shortcut);
+    static inline void *gm__protected(UdonParser *p);
+    static inline void *gm__label    (UdonParser *p);
 
-    static inline int udon_error_value() {
-        return (int)udon_global_error;
-    }
+    static inline void *udon_new_value_node(UdonParser *p);
+    static inline void *udon_new_full_node (UdonParser *p);
 
 // (for c++ inclusion)
 #ifdef __cplusplus
