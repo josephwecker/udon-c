2067da7189ec2e5d0266e16578b156f6a6bbf215
Joseph Wecker <joseph.wecker@gmail.com>
2011-12-26 18:18:57 -0800

    Remove older benchmarking loop and do correct error handling with real parse interface.

--- src/udon2xml.c
+++ src/udon2xml.c
@@ -4,18 +4,16 @@
 
 int main (int argc, char *argv[]) {
     int i;
-    int found = 0;
     if(argc < 2) return 1;
     UdonParser *udon = udon_new_parser_from_file(argv[1]);
     if(udon == NULL) {
         udon_emit_error(stderr);
         return udon_error_value();
     }
-    for(i=0; i<10000; i++) {
-        found += udon_parse(udon);
-        udon_reset_parser(udon);
-    }
+    int res = udon_parse(udon);
+    if(res) udon_emit_error(stderr);
+    udon_reset_parser(udon);
     udon_free_parser(udon);
-    printf("%d\n", found);
+    return res;
 }
 
