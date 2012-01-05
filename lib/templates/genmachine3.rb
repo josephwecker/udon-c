a22638d1aafe49bc9fbcae331658956105ff9ef0
Joseph Wecker <jwecker@justin.tv>
2012-01-04 17:41:26 -0800

    Some cleanup and trying to do variable typing & initializing at the right places.

--- lib/templates/genmachine3.rb
+++ lib/templates/genmachine3.rb
@@ -8,7 +8,7 @@ class GM
     @body = []
     @epoints = []
     @checked_newline = false
-    @allocators = []
+    @allocators = {}
   end
 
   def checked_newline?() return @checked_newline end
@@ -58,58 +58,47 @@ class GM
   end
 
   def allocator(for_type)
-    # TODO: YOU ARE HERE:
-    #   return the function name (with parens) that allocates the type
-    #   register the allocator in a list of allocators that get emitted later.
-    #   Go back and work on "register_return_type"
+    fname = '_new_' + @name.downcase + '_' + for_type.downcase
+    @allocators[for_type] = fname
+    return fname + '()'
   end
 
   def type_name(n)      @name.capitalize         + n end
   def const_name(n)     @name.upcase+'_'         + n end
-  def function_name(n)  '_' + @name.downcase     + n end
-  def allocator_name(n) '_new_' + @name.downcase + n end
+  def function_name(n)  '_' + @name.downcase+'_' + n end
 
   def emit; @body.each{|b| b.emit; puts "\n\n"} end
 end
 
 class GMGenericChild
-  attr_accessor :parent, :name, :body, :locals, :allocators
+  attr_accessor :parent, :name, :body
   def initialize(parent, name)
     @parent = parent
     @name = name
     @body = []
-    if @parent.methods.include? :locals
-      @locals = @parent.locals
-    else
-      @locals = []
-    end
-
-    @allocators = @parent.allocators
   end
-
   def checked_newline?() return @checked_newline end
   def checked_newline=(v) @checked_newline = v; @parent.checked_newline = v end
-
   def gm
     p = @parent
     while !p.is_a?(GM); p = p.parent end
     return p
   end
-
-  def parse(&src)
-    raise "OVERRIDE ME"
+  def gmf
+    p = @parent
+    while !p.is_a?(GMFunction); p = p.parent end
+    return p
   end
-
-  def emit(indent=0)
-    raise "OVERRIDE ME"
-  end
+  def parse(&src) raise "OVERRIDE ME" end
+  def emit(indent=0) raise "OVERRIDE ME" end
 end
 
 class GMCommand < GMGenericChild
-  attr_accessor :parent
+  attr_accessor :ignore
   def initialize(parent, cmd)
     cmd       = cmd.strip
     super(parent, cmd)
+    @ignore   = false
     @cmd      = nil
     @operator = nil
     @lside    = nil
@@ -121,73 +110,77 @@ class GMCommand < GMGenericChild
       lside, rside = cmd.split(@operator)
       @lside = parse_side(lside)
       @rside = parse_side(rside)
+      if @operator == '='
+        # Assignment: might need to make sure local variable exists etc.
+        if @rside[0] == :number
+          ltype = 'uint64_t'
+        elsif @rside[0] == :type
+        if @parent.is_a?(GMFunction)
+          if
+          @parent.register_local(
+      end
     else
       @cmd = parse_side(cmd)
     end
   end
     
   def emit(indent)
-    print '    '*indent
-    if @cmd
-      puts outv(@cmd) + ";"
-    elsif @operator == :advance
-      if @parent.checked_newline?
-        puts "p->column = 1; p->line ++; p->curr ++;"
-        @parent.checked_newline = false;
-      else
-        puts "p->column ++; p->curr ++;"
-      end
-    elsif @operator == '='
-      if (@rside[0] == :number && !@parent.locals.include?(outv(@lside)))
-        puts "uint64_t  #{outv(@lside).ljust(20-10,' ')} = #{outv(@rside)};"
-      else
-        puts "#{outv(@lside).ljust(20,' ')} = #{outv(@rside)};"
-      end
-    elsif @operator
-      puts "#{outv(@lside).ljust(20,' ')} #{@operator} #{outv(@rside)}"
-    else
-      raise "Can't handle this command: '#{@name}'"
-    end
+    #print '    '*indent
+    #if @cmd
+    #  puts outv(@cmd) + ";"
+    #elsif @operator == :advance
+    #  if @parent.checked_newline?
+    #    puts "p->column = 1; p->line ++; p->curr ++;"
+    #    @parent.checked_newline = false;
+    #  else
+    #    puts "p->column ++; p->curr ++;"
+    #  end
+    #elsif @operator == '='
+      #if (@rside[0] == :number && !@parent.loc[outv(@lside)])
+      #  puts "uint64_t  #{outv(@lside).ljust(20-10,' ')} = #{outv(@rside)};"
+      #else
+   #     puts "#{outv(@lside).ljust(20,' ')} = #{outv(@rside)};"
+      #end
+   # elsif @operator
+   #   puts "#{outv(@lside).ljust(20,' ')} #{@operator} #{outv(@rside)}"
+   # else
+   #   raise "Can't handle this command: '#{@name}'"
+   # end
   end
 
-  def outv(statement)
-    stype, sval = statement
-    if [:general, :number, :location].include?(stype)
-      return sval
-    elsif stype == :const
-      return gm.const_name(sval)
-    elsif stype == :type
-      return gm.type_name(sval)
-    elsif stype == :acc
-      if sval[0] == 'mark'
-        return "#{sval[1]}_start = p->curr"
-      else
-        return "#{(sval[1]+'_length').ljust(20,' ')} = p->curr - #{sval[1]}_start"
-      end
-    end
-  end
+  #def outv(statement)
+  #  stype, sval = statement
+  #  if [:general, :number, :location].include?(stype); return sval
+  #  elsif stype == :const; return gm.const_name(sval)
+  #  elsif stype == :type;  return gm.type_name(sval)
+  #  elsif stype == :acc
+  #    if sval[0] == 'mark'; return "#{sval[1]}_start = p->curr"
+  #    else return "#{(sval[1]+'_length').ljust(20,' ')} = p->curr - #{sval[1]}_start" end
+  #  end
+  #end
 
   def parse_side(cmd)
     return nil if cmd.nil?
-    cmd = cmd.strip.
-      gsub(/(?=^|[^a-zA-Z0-9])S(?=$|[^a-zA-Z0-9])/u,   'self_res').
-      gsub(/(?=^|[^a-zA-Z0-9])LINE(?=$|[^a-zA-Z0-9])/u,'p->line').
-      gsub(/(?=^|[^a-zA-Z0-9])COL(?=$|[^a-zA-Z0-9])/u, 'p->column').
-      gsub(/(?=^|[^a-zA-Z0-9])CURR(?=$|[^a-zA-Z0-9])/u,'*(p->curr)').
-      gsub(/(?=^|[^a-zA-Z0-9])C(?=$|[^a-zA-Z0-9])/u,   'self_res->children').
-      gsub(/(?=^|[^a-zA-Z0-9])α(?=$|[^a-zA-Z0-9])/u,   'greek_alpha').
-      gsub(/(?=^|[^a-zA-Z0-9])β(?=$|[^a-zA-Z0-9])/u,   'greek_beta')
+    cmd = specials_map(cmd)
+    if cmd =~ /(?<=^|[^a-zA-Z0-9])(MARK|END)\s*\(([^\)]*)\)(?=$|[^a-zA-Z0-9])/u
+                                             return [:acc,      [$1.downcase, $2]]
+    elsif cmd.start_with?('/')             ; return [:location, gm.parse_location(cmd)]
+    elsif cmd =~ /^[A-Z]+[a-z]+[a-zA-Z]*$/ ; return [:type,     cmd]
+    elsif cmd =~ /^[A-Z]+$/                ; return [:const,    cmd]
+    elsif cmd =~ /^[0-9.]+$/               ; return [:number,   cmd]
+    elsif cmd =~ /^p->(line|col)/          ; return [:number,   cmd]
+    else                                     return [:general,  cmd] end
+  end
 
-    if cmd.start_with?('/')                ; return [:location, gm.parse_location(cmd)]
-    elsif cmd =~ /(?<=^|[^a-zA-Z0-9])(MARK|END)\s*\(([^\)]*)\)(?=$|[^a-zA-Z0-9])/u
-      return [:acc, [$1.downcase, $2]]
-    elsif cmd =~ /^[A-Z]+[a-z]+[a-zA-Z]*$/ ; return [:type, cmd]
-    elsif cmd =~ /^[A-Z]+$/                ; return [:const, cmd]
-    elsif cmd =~ /^[0-9.]+$/               ; return [:number, cmd]
-    elsif cmd.start_with?('p->line') or cmd.start_with?('p->column')
-      return [:number, cmd]
+  def specials_map(cmd)
+    map = [['S',  'self_res'],           ['LINE', 'p->line'],
+           ['COL','p->column'],          ['CURR', '*(p->curr)'],
+           ['C',  'self_res->children'], ['α',    'greek_alpha'],
+           ['β',  'greek_beta']]
+    map.reduce(cmd.strip) do |val, mapping|
+      from, to = mapping
+      val.gsub(/(?=^|[^a-zA-Z0-9])#{from}(?=$|[^a-zA-Z0-9])/u, to)
     end
-    return [:general, cmd]
   end
 end
 
@@ -201,9 +194,8 @@ class GMFunction < GMGenericChild
   def initialize(parent, name, return_type, inner=false)
     # Locals: type, name, declaration position, optional initialization value
     super(parent, name)
-    @local_definitions = []
+    @locals = {}
     if inner
-      #@locals << ['void *', 'self_res'] # TODO: unless "skip" set somehow
       @ftype = register_return_type(return_type)
     else
       @ftype = return_type
@@ -211,11 +203,20 @@ class GMFunction < GMGenericChild
   end
 
   def register_return_type(rt)
-    if rt == 'STRING'
-      @locals << 'self_res'
-      @local_definitions << ['GMString *', 'self_res', gm.allocator('GMString')]
-      @body << GMCommand.new
+    return if rt.nil?
+    if rt == 'STRING'; base_type = 'GMString'
+    elsif rt =~ /^[A-Z]+[a-z]+[a-zA-Z]*$/ ; base_type = gm.type_name(rt) end
+    register_local(base_type +' *', 'self_res', gm.allocator(base_type), false)
+    return "static inline #{base_type} *"
   end
+
+  def register_local(ltype, lname, linitval=nil, prefix_it=true)
+    lname = localname(lname) if prefix_it
+    return if @locals[lname]
+    @locals[lname] = [ltype, linitval]
+  end
+
+  def localname(n) '_l_'+n end
 
   def parse(&src)
     c = yield
