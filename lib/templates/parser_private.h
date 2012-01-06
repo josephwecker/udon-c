70ab4326cf596881f05a687b0aa68179e2266a37
Joseph Wecker <joseph.wecker@gmail.com>
2012-01-05 19:17:52 -0800

    Setting up for actual parser source file generation and general cleanup

---
+++ lib/templates/parser_private.h
@@ -0,0 +1,19 @@
+/* Generated !{datetime} by GenMachine */
+#ifndef __{{parser|up}}_PRIVATE_H__
+#define __{{parser|up}}_PRIVATE_H__
+#ifndef __cplusplus
+//extern "C" {
+#endif
+
+#define _XOPEN_SOURCE 700
+#define _REENTRANT
+
+#include "{{parser}}.h"
+
+/* --- Error handling --- */
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ lib/templates/parser_public.h
@@ -0,0 +1,36 @@
+/* Generated !{datetime} by GenMachine */
+#ifndef __{{parser|up}}_H__
+#define __{{parser|up}}_H__
+#ifndef __cplusplus
+//extern "C" {
+#endif
+
+/* --- Allow setting different malloc/free routines --- */
+#ifdef _{{parser|up}}_NONSTANDARD_MEMORY
+void *{{parser}}_calloc(size_t count, size_t size);
+void *{{parser}}_malloc(size_t size);
+void  {{parser}}_free(void *ptr, size_t size);
+#else
+
+#define {{parser}}_calloc(count,size) calloc(count,size)
+#define {{parser}}_malloc(size)       malloc(size)
+#define {{parser}}_free(ptr,size)     free(ptr)
+#endif
+
+/* --- Error values --- */
+#include <sysexits.h>
+#define {{parser|up}}_OK             EX_OK
+#define {{parser|up}}_MEMORY_ERR     EX_OSERR
+#define {{parser|up}}_FILE_OPEN_ERR  EX_NOINPUT
+#define {{parser|up}}_FILE_READ_ERR  EX_IOERR
+#define {{parser|up}}_DATA_ERR       EX_DATAERR
+
+/* --- Global parser error state --- */
+extern int {{parser}}_global_error;
+extern char {{parser}}_global_error_msg[128];
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
